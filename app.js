9490bb030c23656b318d424fc6432319159dff63
hypothete <probablyduncan@gmail.com>
2022-06-12 12:34:54 -0700

    added drag and drop to image

--- app.js
+++ app.js
@@ -7,43 +7,71 @@ const ctx = can.getContext('2d');
 const imgUrl = './ppanic.png';
 let img;
 let ts = 0;
+let animFrame;
+let imgWidth;
+let imgHeight;
+
+can.addEventListener('dragover', evt => {
+  evt.preventDefault();
+});
+
+can.addEventListener('drop', handleDrop);
 
 loadImage();
 
-async function loadImage() {
+async function loadImage(src) {
   img = new Image();
-  img.src = imgUrl;
+  img.src = src || imgUrl;
   await img.decode();
-  can.width = img.width;
-  can.height = img.height * 2;
-  animate(ts);
+  imgHeight = Math.min(img.height, 200);
+  imgWidth = Math.floor(imgHeight * (img.width / img.height));
+  can.width = imgWidth;
+  can.height = imgHeight * 2;
+  if(!animFrame) {
+    animate(ts);
+  }
 }
 
 function drawReflection() {
-  ctx.drawImage(img, 0, 0);
-  for (let i=0; i <= img.height; i++) {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0,0,can.width, can.height);
+  ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+  for (let i=0; i <= imgHeight; i++) {
     // determine y to sample
-    const offset = (img.height - i - 1);
-    const ratio = i / img.height;
+    const offset = (imgHeight - i - 1);
+    const ratio = i / imgHeight;
     const amplitude = Number(ampSrc.value) * ratio;
     const waveSpeed = Number(waveSrc.value) / 10000;
     const frequency =  Math.sin(Number(freqSrc.value) / (ratio) + (ts * waveSpeed));
     const reflection = amplitude * frequency;
     let y = Math.floor(offset + reflection); // int sampling points only
-    y = Math.min(Math.max(y, 0), img.height); // clamp to source image
+    y = Math.min(Math.max(y, 0), imgHeight); // clamp Y
 
     if (debugSrc.checked) {
       // debug code for viewing wave offsets
       ctx.fillStyle = `rgb(${reflection + 128},${reflection + 128},${reflection + 128})`;
-      ctx.fillRect(0, img.height + i, can.width, 1);
+      ctx.fillRect(0, imgHeight + i, imgWidth, 1);
     } else {
-      ctx.drawImage(can, 0, y, can.width, 1, 0, img.height - 1 + i, can.width, 1);
+      ctx.drawImage(can, 0, y, imgWidth, 1, 0, imgHeight - 1 + i, imgWidth, 1);
     }
   }
 }
 
 function animate(timestamp) {
-  requestAnimationFrame(animate);
+  animFrame = requestAnimationFrame(animate);
   drawReflection();
   ts = timestamp;
+}
+
+function handleDrop(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+
+  const { files } = evt.dataTransfer;
+  const file = files[0];
+  if (file) {
+    const fileUrl = URL.createObjectURL(file);
+    loadImage(fileUrl);
+  }
+  return false;
 }
